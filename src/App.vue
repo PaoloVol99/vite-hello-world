abed5602915d82e8a184a44df3e5bd4f4fbc6323
“PaoloVol99” <paolo.volpini99@gmail.com>
2023-02-28 16:04:36 +0100

    FEAT BONUS jumbotron added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
 
 export default {
   components: {
-    AppTitle
+    AppTitle,
+    AppHero
   },
   data() {
     return {
@@ -14,14 +16,8 @@ export default {
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <AppHero />
+  
   <AppTitle />
 </template>
 
---
+++ src/components/AppHero.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="jumbo">
+        <h1>{{ jumbotronTitle }}</h1>
+        <h3>{{ jumbotronSubtitle }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jumbotronTitle: 'Questo è un jumbotron',
+            jumbotronSubtitle: 'Se ho capito bene'
+        }
+    }
+}
+
+</script>
+
+<style>
+
+.jumbo {
+    background-image: url('/jumbo-sfondo.webp');
+    width: 900px;
+    padding: 110px;
+}
+
+.jumbo h1,
+.jumbo h3 {
+    background-color: rgba(0, 0, 0, 0.5);
+    line-height: 150px;
+}
+
+.jumbo h3 {
+    line-height: 100px;
+    font-size: 30px;
+}
+
+</style>
